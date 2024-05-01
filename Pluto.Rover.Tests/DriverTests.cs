f35eb5a4f7aeb8c488e01856f72ee568da00ea50
Eduard Seuruk <Eduard.Seuruk@gmail.com>
2024-05-01 11:27:14 +0100

    Remove Moq from test project

--- Pluto.Rover.Tests/DriverTests.cs
+++ Pluto.Rover.Tests/DriverTests.cs
@@ -6,128 +6,120 @@ public class DriverTests
     [TestCase("b")]
     [TestCase("l")]
     [TestCase("r")]
-    public void DoNotSupportLowercase(string commands)
+    public void DoNotSupportLowercase(string invalidCommands)
     {
-        var rover = CreateRoverMock(true, true);
-        var driver = new Driver(rover.Object);
+        var rover = new TestRover(true, true);
+        var driver = new Driver(rover);
 
-        Assert.Throws<InvalidOperationException>(() => driver.Move(commands));
-
-        rover.Verify(r => r.TryMoveForward(), Times.Never);
-        rover.Verify(r => r.TryMoveBackward(), Times.Never);
-        rover.Verify(r => r.TakeLeft(), Times.Never);
-        rover.Verify(r => r.TakeRight(), Times.Never);
+        Assert.Multiple(() =>
+        {
+            Assert.Throws<InvalidOperationException>(() => driver.Move(invalidCommands));
+            Assert.That(rover.GetExecutedCommands(), Is.EqualTo(""), "No commands should be executed");
+        });
     }
 
-    [TestCase("", 0, 0, 0, 0)]
-
-    [TestCase("F", 1, 0, 0, 0)]
-    [TestCase("B", 0, 1, 0, 0)]
-    [TestCase("L", 0, 0, 1, 0)]
-    [TestCase("R", 0, 0, 0, 1)]
-
-    [TestCase("FFFFFFFF", 8, 0, 0, 0)]
-    [TestCase("BBBBBBBB", 0, 8, 0, 0)]
-    [TestCase("LLLLLLLL", 0, 0, 8, 0)]
-    [TestCase("RRRRRRRR", 0, 0, 0, 8)]
-
-    [TestCase("FBLR", 1, 1, 1, 1)]
-    [TestCase("BLRF", 1, 1, 1, 1)]
-    [TestCase("LRFB", 1, 1, 1, 1)]
-    [TestCase("RFBL", 1, 1, 1, 1)]
-
-
-    [TestCase("FFFBBBLLLRRR", 3, 3, 3, 3)]
-    [TestCase("FBLRFBLRFBLR", 3, 3, 3, 3)]
-    public void Move(string commands, int stepsForward, int stepsBackward, int turnsLeft, int turnsRight)
+    public void NotMoveOnEmptyComandList()
     {
-        var rover = CreateRoverMock(true, true);
-        var driver = new Driver(rover.Object);
+        var rover = new TestRover(true, true);
+        var driver = new Driver(rover);
 
-        driver.Move(commands);
+        driver.Move("");
 
-        rover.Verify(r => r.TryMoveForward(), Times.Exactly(stepsForward));
-        rover.Verify(r => r.TryMoveBackward(), Times.Exactly(stepsBackward));
-        rover.Verify(r => r.TakeLeft(), Times.Exactly(turnsLeft));
-        rover.Verify(r => r.TakeRight(), Times.Exactly(turnsRight));
+        Assert.That(rover.GetExecutedCommands(), Is.EqualTo(""), "No commands should be executed");
     }
 
-    [TestCase("FRLB", 1, 0, 0, 0)]
-    [TestCase("RLBF", 1, 1, 1, 1)]
-    [TestCase("RLBFRLB", 1, 1, 1, 1)]
+    [TestCase("F")]
+    [TestCase("B")]
+    [TestCase("L")]
+    [TestCase("R")]
 
-    [TestCase("FFFRLB", 1, 0, 0, 0)]
-    [TestCase("RLBFFF", 1, 1, 1, 1)]
-    [TestCase("RLBFFFRLB", 1, 1, 1, 1)]
+    [TestCase("FFFFFFFF")]
+    [TestCase("BBBBBBBB")]
+    [TestCase("LLLLLLLL")]
+    [TestCase("RRRRRRRR")]
 
-    [TestCase("FA", 1, 0, 0, 0)]
-    [TestCase("FRLBA", 1, 0, 0, 0)]
-    [TestCase("FRLBARLB", 1, 0, 0, 0)]
-    public void MoveUntilObstacleAtFront(string commands, int stepsForward, int stepsBackward, int turnsLeft, int turnsRight)
+    [TestCase("FBLR")]
+    [TestCase("BLRF")]
+    [TestCase("LRFB")]
+    [TestCase("RFBL")]
+
+    [TestCase("FFFBBBLLLRRR")]
+    [TestCase("FBLRFBLRFBLR")]
+    public void MoveWithoutObstacles(string requestedCommands)
     {
-        var rover = CreateRoverMock(false, true);
-        var driver = new Driver(rover.Object);
+        var rover = new TestRover(true, true);
+        var driver = new Driver(rover);
 
-        driver.Move(commands);
+        driver.Move(requestedCommands);
 
-        rover.Verify(r => r.TryMoveForward(), Times.Exactly(stepsForward));
-        rover.Verify(r => r.TryMoveBackward(), Times.Exactly(stepsBackward));
-        rover.Verify(r => r.TakeLeft(), Times.Exactly(turnsLeft));
-        rover.Verify(r => r.TakeRight(), Times.Exactly(turnsRight));
+        Assert.Multiple(() =>
+        {
+            Assert.That(rover.GetExecutedCommands(), Is.EqualTo(requestedCommands), "All command should be executed");
+            Assert.That(rover.GetBlockedCommands(), Is.EqualTo(""), "No commands should be blocked");
+        });
     }
 
-    [TestCase("BRLF", 0, 1, 0, 0)]
-    [TestCase("RLFB", 1, 1, 1, 1)]
-    [TestCase("RLFBRLF", 1, 1, 1, 1)]
+    [TestCase("FRLB", "")]
+    [TestCase("RLBF", "RLB")]
+    [TestCase("RLBFRLB", "RLB")]
 
-    [TestCase("BBBRLF", 0, 1, 0, 0)]
-    [TestCase("RLFBBB", 1, 1, 1, 1)]
-    [TestCase("RLFBBBRLF", 1, 1, 1, 1)]
+    [TestCase("FFFRLB", "")]
+    [TestCase("RLBFFF", "RLB")]
+    [TestCase("RLBFFFRLB", "RLB")]
 
-    [TestCase("BA", 0, 1, 0, 0)]
-    [TestCase("BRLFA", 0, 1, 0, 0)]
-    [TestCase("BRLFARLF", 0, 1, 0, 0)]
-    public void MoveUntilObstacleAtBack(string commands, int stepsForward, int stepsBackward, int turnsLeft, int turnsRight)
+    public void MoveUntilObstacleAtFront(string requestedCommands, string executedCommands)
     {
-        var rover = CreateRoverMock(true, false);
-        var driver = new Driver(rover.Object);
+        var rover = new TestRover(false, true);
+        var driver = new Driver(rover);
 
-        driver.Move(commands);
+        driver.Move(requestedCommands);
 
-        rover.Verify(r => r.TryMoveForward(), Times.Exactly(stepsForward));
-        rover.Verify(r => r.TryMoveBackward(), Times.Exactly(stepsBackward));
-        rover.Verify(r => r.TakeLeft(), Times.Exactly(turnsLeft));
-        rover.Verify(r => r.TakeRight(), Times.Exactly(turnsRight));
+        Assert.Multiple(() =>
+        {
+            Assert.That(rover.GetExecutedCommands(), Is.EqualTo(executedCommands), "Only commands from the list should be executed");
+            Assert.That(rover.GetBlockedCommands(), Is.EqualTo("F"), "Should stop at first F");
+        });
     }
 
+    [TestCase("BRLF", "")]
+    [TestCase("RLFB", "RLF")]
+    [TestCase("RLFBRLF", "RLF")]
 
-    [TestCase("A", 0, 0, 0, 0)]
-    [TestCase("AQWE", 0, 0, 0, 0)]
-    [TestCase("AFQBWLER", 0, 0, 0, 0)]
-    [TestCase("AFBLR", 0, 0, 0, 0)]
+    [TestCase("BBBRLF", "")]
+    [TestCase("RLFBBB", "RLF")]
+    [TestCase("RLFBBBRLF", "RLF")]
 
-    [TestCase("FBLRA", 1, 1, 1, 1)]
-    [TestCase("FBALR", 1, 1, 0, 0)]
-    [TestCase("FBALRQFR", 1, 1, 0, 0)]
-    public void FailWithUnknownCommand(string commands, int stepsForward, int stepsBackward, int turnsLeft, int turnsRight)
+    public void MoveUntilObstacleAtBack(string requestedCommands, string executedCommands)
     {
-        var rover = CreateRoverMock(true, true);
-        var driver = new Driver(rover.Object);
+        var rover = new TestRover(true, false);
+        var driver = new Driver(rover);
 
-        Assert.Throws<InvalidOperationException>(() => driver.Move(commands));
+        driver.Move(requestedCommands);
 
-        rover.Verify(r => r.TryMoveForward(), Times.Exactly(stepsForward));
-        rover.Verify(r => r.TryMoveBackward(), Times.Exactly(stepsBackward));
-        rover.Verify(r => r.TakeLeft(), Times.Exactly(turnsLeft));
-        rover.Verify(r => r.TakeRight(), Times.Exactly(turnsRight));
+        Assert.Multiple(() =>
+        {
+            Assert.That(rover.GetExecutedCommands(), Is.EqualTo(executedCommands), "Only commands from the list should be executed");
+            Assert.That(rover.GetBlockedCommands(), Is.EqualTo("B"), "Should stop at first B");
+        });
     }
 
-    private Mock<IRover> CreateRoverMock(bool allowMoveForwared, bool allowMoveBackward)
-    {
-        var rover = new Mock<IRover>();
-        rover.Setup(r => r.TryMoveForward()).Returns(allowMoveForwared);
-        rover.Setup(r => r.TryMoveBackward()).Returns(allowMoveBackward);
+    [TestCase("A", "")]
+    [TestCase("AQWE", "")]
+    [TestCase("AFQBWLER", "")]
+    [TestCase("AFBLR", "")]
 
-        return rover;
+    [TestCase("FBLRA", "FBLR")]
+    [TestCase("FBALR", "FB")]
+    [TestCase("FBALRQFR", "FB")]
+    public void FailWithUnknownCommand(string requestedCommands, string executedCommands)
+    {
+        var rover = new TestRover(true, true);
+        var driver = new Driver(rover);
+
+        Assert.Multiple(() =>
+        {
+            Assert.Throws<InvalidOperationException>(() => driver.Move(requestedCommands));
+            Assert.That(rover.GetExecutedCommands(), Is.EqualTo(executedCommands), "Commands before unknown should be executed");
+        });
     }
 }
---
+++ Pluto.Rover.Tests/TestRover.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Pluto.Rover.Tests;
+
+public class TestRover : IRover
+{
+    private StringBuilder executedCommands = new StringBuilder();
+    private StringBuilder blockedCommands = new StringBuilder();
+
+    public bool AllowMoveForwared { get; }
+
+    public bool AllowMoveBackward { get; }
+
+    public Coordinate Position { get; } = new Coordinate(0, 0);
+
+    public ViewDirection Direction { get; } = ViewDirection.North;
+
+    public TestRover(bool allowMoveForwared, bool allowMoveBackward)
+    {
+        AllowMoveForwared = allowMoveForwared;
+        AllowMoveBackward = allowMoveBackward;
+    }
+
+    public string GetExecutedCommands() { return executedCommands.ToString(); }
+
+    public string GetBlockedCommands() { return blockedCommands.ToString(); }
+
+    public void TakeLeft()
+    {
+        executedCommands.Append('L');
+    }
+
+    public void TakeRight()
+    {
+        executedCommands.Append('R');
+    }
+
+    public bool TryMoveBackward()
+    {
+        if (!AllowMoveBackward)
+        {
+            blockedCommands.Append('B');
+            return false;
+        }
+
+        executedCommands.Append('B');
+        return true;
+    }
+
+    public bool TryMoveForward()
+    {
+        if (!AllowMoveForwared)
+        {
+            blockedCommands.Append('F');
+            return false;
+        }
+
+        executedCommands.Append('F');
+        return true;
+    }
+}
